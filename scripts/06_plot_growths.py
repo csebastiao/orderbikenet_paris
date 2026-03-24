cc00395206acdb2c62c789e5237f0bed60851d4f
Clement Sebastiao <clement.sebastiao@gmail.com>
2026-03-24 11:59:50 +0100

    Fix real growth and explore subtractibe results

--- scripts/06_plot_growths.py
+++ scripts/06_plot_growths.py
@@ -4,8 +4,9 @@ Plot every additive growth strategies.
 
 import json
 import os
+import networkx as nx
 import osmnx as ox
-from orderbike.plot import plot_growth
+from orderbike.plot import plot_growth, plot_graph
 
 FOLDEROOTS = "./data/processed/paris_simplified_results/"
 FILENAME = "paris_cleaned_multigraph.graphml"
@@ -32,6 +33,8 @@ MET_LIST = [
     "betweenness",
     "closeness",
 ]
+
+# TODO add a way to plot also all relevant metrics below or somewhere, show the current step, and the number of kilometers built
 
 
 def main():
@@ -89,29 +92,49 @@ def main():
     folderrealplot = folderplot + "real/"
     if not os.path.exists(folderrealplot):
         os.makedirs(folderrealplot)
-    with open(FOLDEROOTS + "No/real/order_growth.json") as f:
-        order_growth = json.load(f)
-    order_growth = [tuple(val) for val in order_growth]
-    with open(FOLDEROOTS + "No/real/metrics_growth.json") as f:
-        metrics_dict = json.load(f)
-    plot_growth(
+    # with open(FOLDEROOTS + "No/real/metrics_growth.json") as f:
+    #     metrics_dict = json.load(f)
+    # Get boundaries for first edge to be in the right bbox
+    fig, ax = plot_graph(
         G,
-        order_growth,
-        folderrealplot,
-        built=built,
-        color_built="firebrick",
-        color_added="steelblue",
-        color_newest="darkgreen",
+        filepath=folderrealplot + "No.png",
         node_size=8,
-        buffer=BUFFER,
-        buff_size=BUFF_SIZE,
-        plot_metrics=PLOT_METRICS,
-        growth_cov=metrics_dict["coverage"],
-        growth_xx=metrics_dict["xx"],
-        growth_dir=metrics_dict["directness"],
-        growth_reldir=metrics_dict["relative_directness"],
         dpi=DPI,
+        show=False,
+        save=True,
+        close=False,
     )
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    closeness = nx.closeness_centrality(G, distance="length")
+    edge_closeness = {}
+    for edge in G.edges:
+        edge_closeness[edge] = (closeness[edge[0]] + closeness[edge[1]]) / 2
+    init_edge = [tuple(max(edge_closeness, key=edge_closeness.get))]
+    G_init = G.edge_subgraph(init_edge)
+    plot_graph(
+        G_init,
+        filepath=folderrealplot + "0.png",
+        node_size=8,
+        dpi=DPI,
+        show=False,
+        save=True,
+        close=True,
+        bbox=[ymin, ymax, xmin, xmax],
+    )
+    for idx, t in enumerate(TIMESTAMPS[:-1]):
+        H = G.edge_subgraph(
+            [e for e in G.edges if G.edges[e]["built"] in TIMESTAMPS[: idx + 1]]
+        )
+        plot_graph(
+            H,
+            filepath=folderrealplot + t + ".png",
+            node_size=8,
+            dpi=DPI,
+            show=False,
+            save=True,
+            close=True,
+        )
 
 
 if __name__ == "__main__":
